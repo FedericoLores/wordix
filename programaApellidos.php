c1461b3838ff7b87d62837e5e3f1e53577904011
dvc0de <federico.lores@est.fi.uncoma.edu.ar>
2023-10-29 15:32:11 -0300

    remplazamos logica repetitiva

--- programaApellidos.php
+++ programaApellidos.php
@@ -86,21 +86,6 @@ function ingresarPalabra (){
     return $rPalabra;
 }
 
-/** pide al usuario un numero, comprueba si es valido y lo retorna, sino pude uno nuevo
- * @param int $NumeroVal
- * @return int
- * 
-*/
-function numeroValido ($rangoValMin, $rangoValMax){
-    echo "Ingrese un numero";
-    $NumeroVal = trim(fgets(STDIN));
-    while ($NumeroVal <= $rangoValMin || $NumeroVal >= $rangoValMax){
-        echo "Ingrese un numero valido: ";
-        $NumeroVal = trim(fgets(STDIN));
-    }
-    return $NumeroVal;
-
-}
 
 /** Solicita al usuario el nombre del jugador y retorna el nombre en minuscula
  * @return string
@@ -338,6 +323,6 @@ function seleccionarOpcion(){
     echo "seleccione una opción por favor \n";
     echo " 1) Jugar eligiendo palabra \n 2) Jugar con palabra aleatoria \n 3) Ver partida \n 4) Ver primer partida ganadora \n 5) Ver estadísticas de un jugador \n 6) Ver lista de partidas \n 7) Agregar una palabra \n 8) Salir";
     /* llama modulo numeroValido */
-    $opcionInput = numeroValido(1, 8);
+    $opcionInput = solicitarNumeroEntre(1, 8);
     return $opcionInput;
 }
